d63edcd470053ce452fdc5532d9d6e230df698e5
DKDILIPKUMAR 777 <dilipmeena080@gmail.com>
2026-04-09 11:55:58 +0530

    updet

--- QuizMaster/Program.cs
+++ QuizMaster/Program.cs
@@ -24,4 +24,9 @@ app.MapControllerRoute(
 name: "default",
 pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 app.Run();
